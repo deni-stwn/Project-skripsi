af56f0c0fd4b3c539b3007848f72ab1a4c7113b8
Deni setiawan <denistwn67@gmail.com>
2025-05-19 10:26:19 +0700

    rename app.py

--- app/__init__.py
+++ app/__init__.py
@@ -17,3 +17,5 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
+
+# app = create_app()
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
 from app import create_app
 
-app =  create_app()
+# Create the app instance
+app = create_app()
+
+# This is what gunicorn will import
+if __name__ == "__main__":
+    app.run()
